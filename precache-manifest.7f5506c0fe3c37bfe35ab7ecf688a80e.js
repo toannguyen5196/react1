119d6468636fefbc0e9e854f1168204ec271c600
toannguyen5196 <toannguyen5196@gmail.com>
2019-11-27 09:20:00 +0700

    Updates

---
+++ precache-manifest.7f5506c0fe3c37bfe35ab7ecf688a80e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee186d420d243ad4f9aef38e75ef6401",
+    "url": "/react1/index.html"
+  },
+  {
+    "revision": "d10012dc4e2ce94141c7",
+    "url": "/react1/static/css/main.5861c755.chunk.css"
+  },
+  {
+    "revision": "a678f4feac53ef0c2be8",
+    "url": "/react1/static/js/2.3493b5f9.chunk.js"
+  },
+  {
+    "revision": "d10012dc4e2ce94141c7",
+    "url": "/react1/static/js/main.08b4c99f.chunk.js"
+  },
+  {
+    "revision": "b6b1407a49ef640d56e6",
+    "url": "/react1/static/js/runtime-main.1f76eb61.js"
+  },
+  {
+    "revision": "bd8808ccf53b237e972d8055e2d68874",
+    "url": "/react1/static/media/img-banner.bd8808cc.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react1/static/media/logo.25bf045c.svg"
+  }
+]);
